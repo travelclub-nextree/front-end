2197f9712755f77c841797ccbfa609ec12a74a3e
BBeedroid <maisy13@naver.com>
2023-12-08 14:22:43 +0900

    refactor: 로그아웃 로직 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,34 @@
 import React, { ReactElement } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { SPRING_API_URL } from "../../config";
 import { HeaderContainer } from "../header";
 import { Button, Title } from "../../styles/theme";
 
 const Header = (): ReactElement => {
     const navigete = useNavigate();
 
-    const handleLogout = (): void => {
-        localStorage.removeItem("token");
-        alert("로그아웃 했습니다.");
-        navigete("/");
+    const handleLogout = async (): Promise<void> => {
+        try {
+            const response = await axios.post(
+                `${SPRING_API_URL}/api/auth/logout`,
+                {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem(
+                            "token",
+                        )}`,
+                    },
+                },
+            );
+
+            localStorage.removeItem("token");
+            alert(response.data.item);
+            navigete("/");
+        } catch (error) {
+            console.error("An error occurred", error);
+            alert("로그아웃에 실패했습니다.");
+        }
     };
 
     const handleLogoClick = (): void => {
